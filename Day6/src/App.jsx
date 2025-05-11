f4d926f3bfa1f9b4118b8ecf6bce921b7ea0bc5a
Your Name <abid.habib.2018@gmail.com>
2025-05-11 22:32:54 +0500

    noob

--- Day6/src/App.jsx
+++ Day6/src/App.jsx
@@ -1,34 +1,42 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [value,setvalue]=useState('Starting')
+
+
+
+const handleSetValue=(event)=>{
+event.preventDefault(); 
+  const value=event.target.value;
+  setvalue(value);
+
+console.log(value);
+
+
+
+}
+const handleShowValue=(event)=>{
+  event.preventDefault();
+  
+
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+    <section>
+        
+<h1>VALUE {value}</h1>
+
+<input type="text" onChange={handleSetValue} /> 
+
+<button onClick={handleShowValue}>
+Submit
+</button>
+    </section>
+
+
   )
 }
 
