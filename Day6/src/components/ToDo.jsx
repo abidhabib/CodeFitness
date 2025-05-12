1ed8f84eda2e9c8b2c18a12b977531839ee515a3
Your Name <abid.habib.2018@gmail.com>
2025-05-12 19:46:07 +0500

    noob

--- Day6/src/components/ToDo.jsx
+++ Day6/src/components/ToDo.jsx
@@ -5,77 +5,96 @@ import React, { useState } from 'react'
 
 
 const ToDo = () => {
-
-    const [items,setItems]=useState([
+const [copy,setCopy]=useState([])
+    const [items, setItems] = useState([
         {
-            id:1,
-            name:"1",
-            isDone:false,
-            details:"1"
+            id: 1,
+            name: "1",
+            isDone: false,
+            details: "1"
         }
     ])
 
-const addItems = ()=>{
+    const addItems = () => {
 
-setItems([
-...items,{
-    id:items.length+1,
-    name:"Task",
-    isDone:false,
-    details:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
-}
-])
-
-
-}
-
-const deleteItem=(id)=>{
-
-setItems(()=>
-    items.filter((i)=>i.id !==   id)
-)
-}
-const updateItem=(id)=>{
-
-console.log(id);
-setItems(items.map((i)=>{
-return i.id==id?{
-
-id:id,
-name:"Updated",
-isDone:true,
-details:"Updated"
-
-}:i 
-
-}))
+        setItems([
+            ...items, {
+                id: items.length + 1,
+                name: "Task",
+                isDone: false,
+                details: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            }
+        ])
 
 
-}
-  return (
-    <section>
+    }
+
+    const deleteItem = (id) => {
+
+        setItems(() =>
+            items.filter((i) => i.id !== id)
+        )
+    }
+    const updateItem = (id) => {
+
+        console.log(id);
+        setItems(items.map((i) => {
+            return i.id == id ? {
+
+                id: id,
+                name: "Updated",
+                isDone: true,
+                details: `${id} is Updated`
+
+            } : i
+
+        }))
 
 
-<h1>
-    TO-DO
-</h1>
-{
-    items.map((i)=>
+    }
+    const copyItem=(item)=>{
 
-<li key={i.id}>{i.name}{i.id} <br /> {i.details} <br /> {i.isDone?"Done":"Not Done"} <button onClick={()=>deleteItem(i.id)}>
-    Delete
-    </button> 
-    <button onClick={()=>updateItem(i.id)}>Update</button>
+    console.log(item);
+
+const copyItem=JSON.stringify(item)
+setCopy(copyItem)
+navigator.clipboard.writeText(copyItem)
+.then(()=>{ 
+    alert("Copied")
     
-    </li>
-    )
-}
-<button onClick={addItems}>
-Add
-</button>
+}).catch(()=>{
+    alert("Failed")
+})
 
-    </section>
-  )
+console.log(copyItem);
+
+
+    }
+    return (
+        <section>
+
+
+            <h1>
+                TO-DO
+            </h1>
+            {
+                items.map((i) =>
+
+                    <li key={i.id}>{i.name}{i.id} <br /> {i.details} <br /> {i.isDone ? "Done" : "Not Done"} <button onClick={() => deleteItem(i.id)}>
+                        Delete
+                    </button>
+                        <button onClick={() => updateItem(i.id)}>Update</button>
+                        <button onClick={()=>copyItem(i)}>Copy</button>
+
+                    </li>
+                )
+            }
+            <button onClick={addItems}>
+                Add
+            </button>
+
+        </section>
+    )
 }
 
 export default ToDo;    
