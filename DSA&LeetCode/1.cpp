a06af8256bd9a84535b22ae8771c0691d13f8d28
Abid Habib <abid.habib.2018@gmail.com>
2025-08-01 11:21:18 -0700

    gcc

--- DSA&LeetCode/1.cpp
+++ DSA&LeetCode/1.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
-using namespace std;
 
-int main() {
-    cout << "Hello, World!";
-    return 0;
-}   
+#include <iostream>
+int main(){
+    int num=11;
+    if(num%2==0){
+        return 1;
+    }
+    else 
+    {
+                std::cout << "HELLO"; // Now this line will run if num is odd
+
+        return 0;
+    }
+}
