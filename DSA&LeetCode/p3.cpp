9fed22a12a2cb5cc5c8aece19ecee4277c140993
Your Name <abid.habib.2018@gmail.com>
2025-08-05 23:43:57 +0500

    -m

--- DSA&LeetCode/p3.cpp
+++ DSA&LeetCode/p3.cpp
@@ -203,8 +203,7 @@ void print11(int n){
     int count =1;
     for (int i=1;i<=n;i++){
         for(int j=1; j<=i; j++){
-            cout<<count++;
-
+            cout<<i-j+1<<" ";
 
         }
         cout<<endl;
