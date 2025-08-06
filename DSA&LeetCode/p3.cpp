b73b55c710560dabba67eb6f09fe12289b32b05e
Your Name <abid.habib.2018@gmail.com>
2025-08-06 22:05:15 +0500

    print12

--- DSA&LeetCode/p3.cpp
+++ DSA&LeetCode/p3.cpp
@@ -1,223 +1,242 @@
 #include <iostream>
 using namespace std;
 
-
-
 // *
 // **
 // ***
 // ****
 // *****
- void print(int n){
+void print(int n)
+{
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            cout<<"*";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
- }
-
- void print2(int n){
-// ----
-// ---
-// --
-// -
-
-for(int i=1; i<=n; i++){
-
-for(int j=1; j<=n-i; j++){
-    cout<<"-";
 }
 
-cout<<endl;
+void print2(int n)
+{
+    // ----
+    // ---
+    // --
+    // -
 
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "-";
+        }
+
+        cout << endl;
+    }
 }
- }
 
-
-
-void print3(int n){
+void print3(int n)
+{
     // *****
     //  ****
     //   ***
     //    **
     //     *
 
+    for (int i = 1; i <= 5; i++)
+    {
 
-    for (int i=1; i<=5;i++){
-
-        for(int j=1; j<=2*n-i;j++){
-            if(j>=n-(i-1)&&j<=n+(i-1)){
-                cout<<"*";
-                
-            }
-            else{
-                cout<<" ";
-            }
-
-        }
-      
-        
-     
-        cout<<endl;
-    }
-      for (int i=1; i<=5;i++){
-
-        for(int j=1; j<=2*n-i;j++){
-            if(j>=n-(5-i)&&j<=n+(5-i)){
-                cout<<"*";
-                
-            }
-            else{
-                cout<<".";
-            }
-
-        }
-      
-        
-     
-        cout<<endl;
-    }
-
-
-}
-void print4(int n){
-    for (int i=1; i<=n;i++){
-
-
-        for(int j=1;j<=n; j++){
-            if(j>=i){
-                cout<<"*";
+        for (int j = 1; j <= 2 * n - i; j++)
+        {
+            if (j >= n - (i - 1) && j <= n + (i - 1))
+            {
+                cout << "*";
             }
             else
-            cout<<" ";
-        }
-        cout<<endl;
-    }
-}
-void print5(int n){
-for(int i=1; i<=n; i++){
-    for(int j=1; j<=n; j++){
-        if(j>=i)
-        cout<<"*";
-
-        else
-        cout<<" ";
-    }
- 
-    cout<<endl;
-}
-
-}
-
-        void print6(int n){
-            for(int i=1; i<=n; i++){
-                for(int j=1; j<=9; j++){
-                    if (j>=6-i && j<=4+i)
-                    {
-                        /* code */
-                        cout<<"*";
-                    }
-                    else
-                    cout<<" ";
-                    
-             
-                }
-                cout<<endl;
+            {
+                cout << " ";
             }
         }
-void print7(int n){
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=9; j++){
-            if(j<=6-i || j>=4+i)
-            cout<<"*";
-            else
-            cout<<" ";
-        }
-        cout<<endl;
+        cout << endl;
     }
+    for (int i = 1; i <= 5; i++)
+    {
 
+        for (int j = 1; j <= 2 * n - i; j++)
+        {
+            if (j >= n - (5 - i) && j <= n + (5 - i))
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << ".";
+            }
+        }
 
+        cout << endl;
+    }
 }
-void print8(int n){
+void print4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n; j++)
+        {
+            if (j >= i)
+            {
+                cout << "*";
+            }
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print5(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j >= i)
+                cout << "*";
+
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void print6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 9; j++)
+        {
+            if (j >= 6 - i && j <= 4 + i)
+            {
+                /* code */
+                cout << "*";
+            }
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print7(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 9; j++)
+        {
+            if (j <= 6 - i || j >= 4 + i)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print8(int n)
+{
     // print this shape
-//     1    
-//    121
-//   12321
-//  1234321
-    for(int i=1; i<=n; i++){
-            int k=1;
+    //     1
+    //    121
+    //   12321
+    //  1234321
+    for (int i = 1; i <= n; i++)
+    {
+        int k = 1;
 
-        for(int j=1; j<=7; j++){
-            if(j>=5-i && j<=3+i){
-                
-                    /* code */ 
-                    cout<<k;
-                                            j<4?k++:k--;
-       
-            }       
-                
-            else
-            cout<<" ";
-       
- 
+        for (int j = 1; j <= 7; j++)
+        {
+            if (j >= 5 - i && j <= 3 + i)
+            {
+
+                /* code */
+                cout << k;
+                j < 4 ? k++ : k--;
             }
-        cout<<endl;
+
+            else
+                cout << " ";
         }
-    
-}
-void print9(int n){
-
-for(int i=1;i<=n;i++){
-for(int j=1;j<=9;j++){
-
-if(j>=7-i && j<=3+i)
-cout<<" ";
-else
-cout<<"*";
-}
-
-cout<<endl;
-
-}
-}
-
-
-void print10(int n){
-
-    for (int i=1;i<=n;i++){
-        for(int j=1; j<=i; j++){
-            cout<<i;
-
-
-        }
-        cout<<endl;
-
-
-
+        cout << endl;
     }
 }
-void print11(int n){
-    int count =1;
-    for (int i=1;i<=n;i++){
-        for(int j=1; j<=i; j++){
-            cout<<i-j+1<<" ";
+void print9(int n)
+{
 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 9; j++)
+        {
+
+            if (j >= 7 - i && j <= 3 + i)
+                cout << ".";
+            else
+                cout << "*";
         }
-        cout<<endl;
 
-
-
+        cout << endl;
     }
 }
 
-int main(){
+void print10(int n)
+{
 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+void print11(int n)
+{
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i - j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+void print12(int n)
+{
+            char a = 'A';
 
+    for (int i = 1; i <= 5; i++)
 
-      int n=4;
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            char x='A'+i-1;
+            cout << x << " ";
+            a++;
+        }
+
+        cout << endl;
+    }
+}
+int main()
+{
+
+    int n = 4;
 
     // print(n);
     // print4(n);
@@ -230,11 +249,7 @@ int main(){
     // cout<<"----------------"<<endl;
     // print9(n);
     // print10(n);
-       print11(n);
-
+    print12(n);
 
     return 0;
-
-
 }
-
