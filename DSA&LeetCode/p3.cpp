bafb811c07b4ecf41c1098bfda3b0719a76420ab
Your Name <abid.habib.2018@gmail.com>
2025-08-06 22:07:05 +0500

    print13

--- DSA&LeetCode/p3.cpp
+++ DSA&LeetCode/p3.cpp
@@ -233,6 +233,22 @@ void print12(int n)
         cout << endl;
     }
 }
+void print13(int n)
+{
+            char a = 'A';
+
+    for (int i = 1; i <= 5; i++)
+
+    {
+        for (int j = 1; j <= i; j++)
+        {;
+            cout << a << " ";
+            a++;
+        }
+
+        cout << endl;
+    }
+}
 int main()
 {
 
@@ -249,7 +265,8 @@ int main()
     // cout<<"----------------"<<endl;
     // print9(n);
     // print10(n);
-    print12(n);
+    // print12(n);
+    print13(n);
 
     return 0;
 }
