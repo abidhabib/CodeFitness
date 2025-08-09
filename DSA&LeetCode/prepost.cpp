1b082f21ec7b924d74ef5e8482e572bc76f8b296
Your Name <abid.habib.2018@gmail.com>
2025-08-09 22:46:47 +0500

    d

--- DSA&LeetCode/prepost.cpp
+++ DSA&LeetCode/prepost.cpp
@@ -4,10 +4,10 @@ int main(){
 
 int i=5;
 
-cout<<(i++)<<endl;
-cout<<(++i)<<endl;
-cout<<(i--)<<endl;
-cout<<(--i)<<endl;
+cout<<(i++)<<endl; //print 5 and than increment(6)
+cout<<(++i)<<endl; // (1)increment and than print (7)
+cout<<(i--)<<endl; // print (7) than decrement 7-1
+cout<<(--i)<<endl; // (6-1)decrement and than print (5)
 
 
 
